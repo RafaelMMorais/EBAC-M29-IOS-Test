5567523d2a70b2c1720e7abfa060827c157fd7cb
Rafael Morais <rafaelm.morais@hotmail.com>
2024-10-10 15:30:56 -0300

    Adicionado aftertest e beforetest

--- wdio.ios.conf.js
+++ wdio.ios.conf.js
@@ -44,22 +44,23 @@ export const config = {
             disableWebdriverScreenshotsReporting: false
         }]
     ],
+    
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
-    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
-        await driver.takeScreenshot();
-    },
     
-    beforeSuite: async function() {
+    //beforeSuite: async function() ==> Abrir o app sempre depois do teste
+    beforeTest: async function() {
         let state = await driver.queryAppState("br.com.lojaebac")
         if(state !== 4){
             await driver.execute('mobile: launchApp', {bundleId: 'br.com.lojaebac'})
         }
     },
 
-    afterSuite: async function() {
+    //afterSuite: async function() ==> Fechar o app sempre depois do teste
+    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+        await driver.takeScreenshot();
         await driver.execute('mobile: terminateApp', {bundleId: 'br.com.lojaebac'})
     }
 }
